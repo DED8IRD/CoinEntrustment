9e716857f4afa492f614a72a0a8e2c619bf5e23a
DED8IRD <ecwu@ucsc.edu>
2017-03-12 14:22:24 -0700

    completed voice script

--- CEGame.py
+++ CEGame.py
@@ -39,6 +39,16 @@ def get_subject_cooperation():
         else:
             print("Invalid. Answer (y/n)")
 
+def print_entrustment_prompt(round):
+    m4v = -1
+    if ROUND == 1:
+        m4v = 1
+    elif 1 < ROUND < 7: 
+        m4v = 3
+    elif ROUND == 7:
+        m4v = 4
+    print('\n\t\t****Play the m4v file that begins with \'' +str(m4v)+ '\'****')
+
 def print_scores():
     if OPPONENT_SCORE > AGENT_SCORE:
         winner = 'SUBJECT ' + PARTICIPANT
@@ -67,6 +77,9 @@ def print_scores():
         outfile.write(str(subject)) 
 
     print(out)
+    print('\n\t\t****Play m4v \'5\' then \'6\'****')
+    print('\n\t\t****If AGENT wins, play m4v \'7\' else play \'8\'****')
+
 
 
 if __name__ == '__main__':
@@ -78,9 +91,19 @@ if __name__ == '__main__':
 
     print('-'*80)
     print('\tThis is the Coin Entrustment Game.')
+    print('\n\tYou are the operator of this game. As operator, you will embody a 3d' \
+        + '\n\tavatar agent named Rovy. You will \"play\" a game with the participant' \
+        + '\n\tas the avatar through a video call. To \"play\" the command-line game,' \
+        + '\n\tsimply follow the prompts and play the recordings specified. You will' \
+        + '\n\tenter participant inputs and the game will calculate your next moves.' \
+        + '\n\tAll you have to do is play the right recordings. The recordings will' \
+        + '\n\tbe specified by their filename number prefix.')
+
     print('-'*80)
 
     PARTICIPANT = raw_input("\t\tEnter participant: ")
+    print('\n\t\t****Play the m4v file that begins with \'0\'****')
+
 
     while ROUND <= NUM_ROUNDS:
         print('-'*80)
@@ -91,6 +114,7 @@ if __name__ == '__main__':
         subject.coins.append(10)
 
         # Entrustment phase
+        print_entrustment_prompt(ROUND)
         if ROUND == 1:
             agent.entrust(subject, 3)
         else:
@@ -105,10 +129,18 @@ if __name__ == '__main__':
         subject_entrust_coins = get_subject_entrustment()
         subject.entrust(agent, subject_entrust_coins)
         
-        print('\t\tSAY: \"I\'m giving you ' +str(agent.trust[-1])+ ' coin(s).\"\n')        
+        print('\n\t\t****Play the m4v file that begins with \'9.1\'****')
+        print('\t\tSAY: ' +str(agent.trust[-1])+ ' coin(s).\"\n')        
         print('\t\t'+'-'*45+'\n')
 
         # Cooperation phase
+        print('\n\t\t****Play the m4v file that begins with \'2\'****')
+        subject_cooperates = get_subject_cooperation()
+        if subject_cooperates:
+            subject.cooperate(agent)
+        else:
+            subject.defect(agent)
+
         if sum(subject.coop[-2:]) < 2 and len(subject.coop) > 1:
             defected = True
         else:
@@ -116,18 +148,15 @@ if __name__ == '__main__':
 
         if not defected:
             agent.cooperate(subject)
+            print('\n\t\t****Play \'9.2\' COOP****')
+
         else:
             agent.defect(subject)
+            print('\n\t\t****Play \'9.2\' DEFECT****')
 
-        subject_cooperates = get_subject_cooperation()
-        if subject_cooperates:
-            subject.cooperate(agent)
-        else:
-            subject.defect(agent)
-
-        print('\t\tSAY: ' + ('\"I\'ll give back your coins.\"\n' if agent.coop[-1] == 1 \
-                        else '\"I\'m keeping your coins.\"\n'))        
-        print('\t\t'+'-'*45)
+        # print('\t\tSAY: ' + ('\"I\'ll give back your coins.\"\n' if agent.coop[-1] == 1 \
+        #                 else '\"I\'m keeping your coins.\"\n'))        
+        # print('\t\t'+'-'*45)
 
         prev_payoff = sum(agent.coins[ROUND*5-4:ROUND*5])
         print('\n\t\tROUND ' +str(ROUND)+ ' RESULTS: ' \
